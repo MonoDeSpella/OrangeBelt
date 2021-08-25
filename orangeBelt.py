480c66c603d392dd55342bda8b0b6b915668a95a
MonoDeSpella <89548691+MonoDeSpella@users.noreply.github.com>
2021-08-25 23:20:22 +0300

    Add some levels
    beatiful symbol-arts

--- orangeBelt.py
+++ orangeBelt.py
@@ -2,3 +2,83 @@
 import random
 
 word_list = ['math', 'literature', 'history', 'programming', 'csharp', 'onelove', 'python']
+
+#function that gets random word from list above (:D)
+def get_word():
+    word = random.choice(word_list)
+    return word.upper()
+  
+# getting current level
+def display_hangman(tries):
+    stages = [  # final stage: head, torso, both hands, both legs
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                ''',
+                # head, torso, both hands, one leg
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                ''',
+                # head, torso, both hands
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                ''',
+                # head, torso, one hand
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                ''',
+                # head and torso
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                ''',
+                # head
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                ''',
+                # first stage
+                '''
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                '''
+    ]
+    return stages[tries]
